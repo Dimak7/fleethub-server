9e998b96fbb02ec314975dc50d15fd7642bb00b6
Dima <dima0704@Dimas-MacBook-Pro.local>
2025-04-08 18:16:58 -0500

    ✅ Fix Firebase admin credentials with env vars

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const admin = require("firebase-admin");
 const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 const db = admin.firestore();
 
